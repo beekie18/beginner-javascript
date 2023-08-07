58e0d13e6e8bb535db96132704dc583d18d314c4
beekie18 <aviv@newmanatlanta.com>
2023-08-07 14:53:56 -0400

    Update mainloop.js

--- sokoban/mainloop.js
+++ sokoban/mainloop.js
@@ -12,3 +12,5 @@ const canvas = document.querySelector('#mainscreen');
 const ctx = canvas.getContext('2d');
 const { width, height } = canvas;
 ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// https://stackoverflow.com/questions/3420975/html5-canvas-zooming
